9df2ec621120bec83f35fd1f590ffe6569baa7d7
SmonFtwi <smonftwi1@gmail.com>
2024-01-28 12:31:17 +0400

    this commit is adding image to skill section and fixing the error caused by the image

--- client/src/pages/images.jsx
+++ client/src/pages/images.jsx
@@ -9,6 +9,10 @@ import webpack from './../assets/webPack.png'
 import git from './../assets/Git.png'
 import express from './../assets/expressjs.png'
 import react from './../assets/react.svg'
+import pandas from './../assets/Pandas.png'
+import numpy from './../assets/numpy.png'
+import sklearn from './../assets/Scikit_learn.png'
+import tensorflow from './../assets/Tensorflow.png'
 
 export const  Images = [
     {
@@ -65,5 +69,25 @@ export const  Images = [
         id: 11,
         img:react,
         desc: "React"
+    },
+    {
+        id: 12,
+        img:pandas,
+        desc: "Pandas"
+    },
+    {
+        id: 12,
+        img:numpy,
+        desc: "Numpy"
+    },
+    {
+        id: 13,
+        img:sklearn,
+        desc: "Sklearn"
+    },
+    {
+        id: 14,
+        img:tensorflow,
+        desc: "Tensorflow"
     },
 ]
